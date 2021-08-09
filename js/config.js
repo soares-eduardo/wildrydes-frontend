a8a9e9b2a683bdbb7f82fbe200ab243c18c6b138
Eduardo André Soares <eduardoandre.mcc@gmail.com>
2021-08-09 10:09:41 -0300

    Setted AWS Congnito authentication.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_vol7lJQES', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '203h2rf7uuode823p3oh4olbeu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
